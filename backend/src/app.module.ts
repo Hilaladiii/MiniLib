471e84acf169cf9e285b058596399ddde4dbe1e3
Hilaladiii <abdulhilal242@gmail.com>
2024-11-17 13:25:30 +0700

    init folder structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { BookModule } from './modules/book/book.module';
+import { BorrowModule } from './modules/borrow/borrow.module';
+import { SupabaseModule } from './modules/supabase/supabase.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookModule, BorrowModule, SupabaseModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
